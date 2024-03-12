ff54b5d199861746da5ad7abaaac052fca90c3f5
revizes <revizes@gmail.com>
2024-03-12 22:27:14 +0900

    arrange...

--- pycounter/counter.py
+++ pycounter/counter.py
@@ -2,21 +2,21 @@ from tkinter import *
 from tkinter import font
 from tkinter import ttk
 
+
 root = Tk()
 root.title("Counter")
 root.geometry("640x400")
 
 style = ttk.Style()
-style.theme_use("vista")
-style.configure("TLabel", font=("Verdana", 150), foreground="red")
-style.configure("TButton", font=("Verdana", 20, 'bold'), foreground="blue")
-style.configure("new.TButton", font=("Verdana", 20, 'bold'), foreground="green")
-
+style.configure("TLabel", font=('Verdana', 150), foreground="red")
+style.configure("Inc.TButton", font=('Verdana', 30), foreground="blue")
+style.configure("Rst.TButton", font=('Verdana', 30), foreground="black")
 
 count = 0
 
-lbl_counter = ttk.Label(root, text=str(count), anchor='center')
+lbl_counter = ttk.Label(root, text=str(count))
 lbl_counter.configure(style='TLabel')
+lbl_counter.configure(anchor="center")
 lbl_counter.pack(side='top', fill='x')
 
 
@@ -28,9 +28,11 @@ def increase_lbl():
 
 
 btn_inc = ttk.Button(root)
-btn_inc.configure(text="+")
-btn_inc.configure(style="TButton")
+btn_inc.config(text='+')
+btn_inc.config(style='Inc.TButton')
 btn_inc.pack(side='left', expand=True, fill='both')
+#btn_inc.pack(side='left', fill='both', anchor='w')
+
 btn_inc.config(command=increase_lbl)
 
 
@@ -42,8 +44,9 @@ def reset_lbl():
 
 btn_reset = ttk.Button(root)
 btn_reset.config(text='0')
-btn_reset.configure(style='new.TButton')
+btn_reset.configure(style='Rst.TButton')
 btn_reset.pack(side='right', expand=True, fill='both')
+
 btn_reset.config(command=reset_lbl)
 
 
@@ -54,7 +57,7 @@ def decrease_lbl():
         lbl_counter.config(text=str(count))
 
 
-def keypress_handler(event):
+def key_handler(event):
     if event.keycode == 27:     # esc
         reset_lbl()
     elif event.keycode == 8:    # backspace
@@ -63,7 +66,6 @@ def keypress_handler(event):
         increase_lbl()
 
 
-root.bind('<Key>', keypress_handler)
-
+root.bind('<Key>', key_handler)
 
 root.mainloop()
